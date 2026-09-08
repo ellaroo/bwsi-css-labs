ab32717df1a3c08227b8061e9bb96dec89abd843
ellaZ <ellaz3696@gmail.com>
2026-09-08 14:14:50 -0400

    Added Personal Introduction; lab1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ella Zeng" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I'm 15 turning 16 this year, and I'm excited to be working on the BWSI Course!")
 
 if __name__ == "__main__":
     main()
